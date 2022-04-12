af3606ca2af6e1e067f42beac66ff666ec978e4b
Geison Amorim <geisonam43@gmail.com>
2022-04-11 21:47:36 -0300

    Add project files.

---
+++ DiferençaDeProduto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Variavel
+
+            int A, B, C, D, dif;
+
+            //Entrada
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            //Processamento
+
+            dif = A * B - C * D;
+
+            //Saida
+
+            Console.WriteLine("DIFERENCA = " + dif);
+
+        }
+    }
+}
